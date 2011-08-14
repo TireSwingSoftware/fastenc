faa7d7b6489cfe73146dca3c6d0a8aba5cd5e791
Michael Hrivnak <mhrivnak@hrivnak.org>
2011-08-13 23:07:20 -0400

    Added a setup.py script for distutils

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fastenc',
+    version='0.1',
+    description='encoding.com library',
+    packages=('fastenc',),
+    license='BSD',
+)
